b7945b4bb13e1de83409810f808522e1bb1b6e0f
aaaabima <aaaabim@gmail.com>
2022-05-17 13:51:46 +0700

    Timer on Level1 and pause key integration

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -34,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode walkKey = KeyCode.LeftShift;
     public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode pauseKey = KeyCode.Escape;
 
     [Header("Ground Check")]
     public float playerHeight;
@@ -163,6 +165,8 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
+        TimerController.instance.BeginTimer();
+
         ResetJump();
 
         startYScale = transform.localScale.y;
@@ -218,6 +222,12 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
 
             crouching = false;
+        }
+
+        // pause game
+        if (Input.GetKey(pauseKey))
+        {
+            SceneManager.LoadScene("PauseScene");
         }
     }
     
---
+++ Assets/Script/TimerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+
+    public TextMeshProUGUI timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerOn;
+
+    private float elapsedTime;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        timeCounter.text = "Time: 00 : 00 . 00";
+        timerOn = false;
+    }
+
+    public void BeginTimer()
+    {
+        timerOn = true;
+        elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void ResumeTimer()
+    {
+        timerOn = true;
+
+        StartCoroutine(UpdateTimer());
+    }
+    public void EndTimer()
+    {
+        timerOn = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerOn)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm' : 'ss' . 'ff");
+            timeCounter.text = timePlayingStr;
+
+            yield return null;
+        }
+    }
+}
